bcc95b70944a005514fddc02b23a51168bd0aae0
zxfjd3g <258147149@qq.com>
2019-08-10 20:30:50 +0800

    ratings

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import {Button} from 'mint-ui'
 
+import './filters'
 import './mock/mock-server'
 import App from './App.vue'
 import store from './store'
@@ -8,14 +9,17 @@ import router from './router'
 import Header from 'components/Header/Header.vue'
 import Star from 'components/Star/Star.vue'
 import CartControl from 'components/CartControl/CartControl.vue'
+import Split from 'components/Split/Split.vue'
 
 
+Vue.prototype.$eventBus = new Vue()
 Vue.config.productionTip = false // 去掉提示输出
 
 // 注册全局组件
 Vue.component('Header', Header)
 Vue.component('Star', Star)
 Vue.component('CartControl', CartControl)
+Vue.component('Split', Split)
 Vue.component(Button.name, Button)  // mt-button
 
 /* new Vue({
